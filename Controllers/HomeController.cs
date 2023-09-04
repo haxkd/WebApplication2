adda68e7bf1afe4be4fe84f80d79fd68f67ca2ed
Abul Hasan <33129246+haxkd@users.noreply.github.com>
2023-09-04 16:48:49 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+            string query = $"INSERT INTO Employee(name,email,gender,password) VALUES ('{emp.UserName}','{emp.UserEmail}','{emp.UserGender}','{emp.UserPassword}')";
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            return View();
+        }       
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserGender { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
